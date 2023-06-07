49f8d0b288b779978c14d43355e86e7f13724402
w2534072030@gmail.com <w2534072030@gmail.com>
2023-06-08 01:29:06 +0800

    no message

--- OverLoad01.java
+++ OverLoad01.java
@@ -1,19 +1,42 @@
-public class OverLoad01 {
+package chapter07.method;
+
+public class Overload01 {
 	public static void main(String[] args) {
-		MyCalculator ma = new MyCalculator();
- 
-		System.out.println(ma.calculate(1,2));
-		
+		Overload mc = new Overload();
+		System.out.println(mc.Overload(1, 1));
+		System.out.println(mc.Overload(2, 2.0));
+		System.out.println(mc.Overload(3.0, 3));
+		System.out.println(mc.Overload(4,4,4));
 	}
+
 }
 
-class MyCalculator {
-	public int calculate(int n1,int n2);{
-	return n1 + n2;
+class Overload {
+	public int Overload(int a1, int b1) {
+		System.out.println("int, int被调用bbbbbbb");
+		//注意：
+		return a1 + b1;
+	} 
+	public double Overload(int n1, double n2) {
+		System.out.println("int double 被调用");
+		return n1 - n2; 
 	}
-	
-	public double calculate(int n2,double n1);{
-		return n2 + n1;
-		}
+	public double Overload(double n1, int n2) { 
+		System.out.println("double6"+ " int被调用.."); 
+	return n1 * n2; 
+	}
+	public int Overload(int n1, int n2,int n3) {
+		System.out.println("int int int 被调用");
+		return n1 + n2 + n2; 
+	} 
 	
 }
+
+
+
+
+
+
+
+
+
